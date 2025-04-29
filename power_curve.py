9d6c79fede42361681fd7a620a0254fa772e1aca
WJMCI <wj2970@mci4me.at>
2025-04-29 16:05:43 +0200

    changes 29.04.2025 16:05

---
+++ power_curve.py
@@ -0,0 +1,26 @@
+from load_data import load_data
+from sort import bubble_sort
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = load_data('activity.csv')
+power_W = data['PowerOriginal']
+sorted_power_W = bubble_sort(power_W)
+def time_array():
+    time=np.arange(0, len(power_W), 1)
+    return time
+print(time_array())
+
+
+def plot_power_curve(sorted_power_W, time_array):
+    plt.figure(figsize=(10, 5))
+    plt.plot(time_array(), sorted_power_W, label='Power Curve', color='blue')
+    plt.title('Power Curve')
+    plt.xlabel('Time (min)')
+    plt.ylabel('Power (W)')
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+plot_power_curve(sorted_power_W, time_array)
+
